af3c6ce97a08f00844cfdd3916bdd1768ca886dd
Breandon <b.cwy@live.com>
2026-05-17 08:49:09 -0700

    Configure backend for cloud database

--- backend/database.py
+++ backend/database.py
@@ -1,7 +1,12 @@
+import os
+
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://notes_user:notes_password@localhost:5432/notes_db"
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 
